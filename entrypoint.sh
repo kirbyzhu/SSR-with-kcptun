ddba8703906c4acaa0de6f4b6693c824b39e0441
kirbyzhu <kirbyzhu@gmail.com>
2017-06-10 17:03:06 +0800

    Create entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,15 +8,4 @@ else
     echo -e "\033[33mKcptun not started......\033[0m"
 fi
 
-echo $SS_MODULE
-echo $SS_CONFIG
-
-echo -e "\033[32mStarting shadowsocks......\033[0m"
-if [ -n "$SS_CONFIG" ]; then
-    $SS_MODULE $SS_CONFIG
-else
-    echo -e "\033[31mError: SS_CONFIG is blank!\033[0m"
-    exit 1
-fi
-
 /usr/bin/python /ssr/shadowsocks/server.py "$@"
